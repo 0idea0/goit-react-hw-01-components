d64b77c384235742a3c60f814fa1aca780f2375c
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2021-11-29 22:16:12 +0200

    add transaction

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+
 import userData from './data/user.json';
 import statistics from './data/data.json';
 import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
       <Profile {...userData} />
       <Statistics title="Upload stats" statistics={statistics} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
